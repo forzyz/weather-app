5bf950a055fccee2bf3964db1b266b1cbfe9db31
Ruslan Telitsyn <zimpuytel@gmail.com>
2023-10-07 21:39:35 +0300

    temperature fetch commit

--- app/component/Input.tsx
+++ app/component/Input.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { type } from "os";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiCurrentLocation } from "react-icons/bi";
 
-const Input = () => {
+type InputProps = {
+  handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  setLocation: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Input = ({ handleSearch, setLocation }: InputProps) => {
   return (
-    <form className="flex items-center md:w-2/4 w-full mb-6">
+    <form className="flex items-center md:w-5/5 w-full mb-6">
       <div className="cursor-pointer mr-2">
         <AiOutlineSearch />
       </div>
@@ -12,6 +18,8 @@ const Input = () => {
         className="w-full bg-transparent outline-none focus:border-b-2 focus:border-gray-400 transition duration-300 ease-in-out"
         type="text"
         placeholder="Search for places..."
+        onKeyDown={handleSearch}
+        onChange={(e) => setLocation(e.target.value)}
       />
       <div className="cursor-pointer ml-2 hover:bg-gray-200 rounded-full p-1 transition duration-300 ease-in-out">
         <BiCurrentLocation />
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,53 @@
 "use client";
+import React, { useState } from "react";
 
 import Input from "./component/Input";
 
 const Home = () => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=8ef2f4e6d12643c1baf161027230110&q=${location}&days=7&aqi=yes&alerts=yes`;
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+        setLocation("");
+        setError("");
+      } catch (error) {
+        setError("City not found");
+        setData({});
+      }
+    }
+  };
+
   return (
     <div className="flex bg-cover bg-slate-300 h-screen">
       <div className="bg-slate-200 rounded-lg flex self-center m-auto w-11/12 h-5/6">
         {/* Left side */}
         <div className="rounded-lg bg-white w-1/3">
           <div className="flex flex-col p-6">
-            <Input />
+            <Input handleSearch={handleSearch} setLocation={setLocation} />
             <h1>Weather Icon</h1>
-            <h1>Temp</h1>
+            {data.current ? (
+              <div>{data.current.temp_c}</div>
+            ) : null}
             <h1>Day</h1>
           </div>
         </div>
         {/* Right side */}
-        <div>
-
-        </div>
+        <div></div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
