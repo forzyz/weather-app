52a89ee238ab930a7407db582170032c82b83625
Ruslan Telitsyn <zimpuytel@gmail.com>
2023-10-08 12:43:40 +0300

    image for weather

--- app/component/Input.tsx
+++ app/component/Input.tsx
@@ -4,12 +4,12 @@ import { BiCurrentLocation } from "react-icons/bi";
 
 const Input = () => {
   return (
-    <form className="flex items-center md:w-2/4 w-full mb-6">
+    <form className="flex items-center  w-full mb-6">
       <div className="cursor-pointer mr-2">
         <AiOutlineSearch />
       </div>
       <input
-        className="w-full bg-transparent outline-none focus:border-b-2 focus:border-gray-400 transition duration-300 ease-in-out"
+        className="w-full bg-transparent outline-none focus:border-b-2 focus:border-gray-400 transition duration-300 ease-in-out placeholder-black"
         type="text"
         placeholder="Search for places..."
       />
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Input from "./component/Input";
 
 const Home = () => {
@@ -7,21 +8,25 @@ const Home = () => {
     <div className="flex bg-cover bg-slate-300 h-screen">
       <div className="bg-slate-200 rounded-lg flex self-center m-auto w-11/12 h-5/6">
         {/* Left side */}
-        <div className="rounded-lg bg-white w-1/3">
+        <div className="rounded-lg bg-white w-2/7">
           <div className="flex flex-col p-6">
             <Input />
-            <h1>Weather Icon</h1>
+            <Image
+              src="/assets/cloudy.png"
+              width={200}
+              height={200}
+              alt="Picture of the author"
+              className="mb-10 "
+            />
             <h1>Temp</h1>
             <h1>Day</h1>
           </div>
         </div>
         {/* Right side */}
-        <div>
-
-        </div>
+        <div></div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
